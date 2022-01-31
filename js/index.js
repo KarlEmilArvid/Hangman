a89cb6d6f71035fee07292fcf2cfa5dcdba640d0
KarlEmilArvid <emilarvidsson96@gmail.com>
2022-01-31 12:03:27 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 /**
+ * TODO:
+ * Word array 3 ord med olika längd
+ * svg array, för varje fel bokstav så visas index 0 till 4
+ * Input med tangentbord, bara gissa på en bokstav i taget
+ * "Du vann" overlay
+ * "Du förlorade" overlay
+ * Input text skickas in i JS filen, räkna bokstäver
+ * 
+ * 
+ * Nedräkning på 1 min
+ */
+
+/**
  För att toggla SVG:en
  document.querySelector('figure').classList.add('scaffold')
  document.querySelector('figure').classList.add('head')
