f8cb0ebe4aa8c96212db65f20bd17d90c6c2fbd7
Felix Stark <felix.b.stark@gmail.com>
2022-02-02 11:53:57 +0100

    animation-works

--- js/index.js
+++ js/index.js
@@ -1,28 +1,18 @@
 /**
  * TODO:
-
- * en text-input för varje bokstav i ordet
- * vid rätt bokstav - (blinka grönt) gå vidare till nästa input
- * vid fel bokstav - (blinka rött) infoga bildanimering och töm input
+ * 
  * uppdatera figur svg array, för varje fel bokstav så visas index 0 till 4
  * Input med tangentbord, bara gissa på en bokstav i taget
- * "Du vann" overlay
- * "Du förlorade" overlay
- 
+ * Gömma input fält
  * 
  * 
  * Nedräkning på 1 min
  */
 
 
-/**
- För att toggla SVG:en
- document.querySelector('figure').classList.add('scaffold')
- document.querySelector('figure').classList.add('head')
- document.querySelector('figure').classList.add('body')
- document.querySelector('figure').classList.add('arms')
- document.querySelector('figure').classList.add('legs')
-*/
+
+
+
 
 let currentWord = [];
 let answerWord = [];
@@ -30,13 +20,14 @@ let pastLetters = [];
 let lettersReset = '';
 let i;
 let guessesLeft = 5;
-document.getElementById("guesses-remain").innerHTML = guessesLeft;
+document.getElementById('guesses-remain').innerHTML = guessesLeft;
+
 
 const wordArray = ['dog', 'apple', 'laptop', 'croiassant'];
 const overlayWin = document.querySelector('.overlay-win');
 const overlayLose = document.querySelector('.overlay-lose');
-const playAgainBtn = document.querySelector('.win-reset-button');
-const rematchBtn = document.querySelector('.lose-reset-button');
+const playAgainBtn = document.querySelector('.play-again');
+const rematchBtn = document.querySelector('.play-again');
 const closeOverlayWin = document.querySelector('.close-win');
 const closeOverlayLose = document.querySelector('.close-lose');
 
@@ -45,8 +36,10 @@ let correctAnswer = wordArray[Math.floor(Math.random() * wordArray.length)];
 let alwaysFocusedInput = document.getElementById( 'activeFocus' ); //Fokus på input fält
 
 //BUTONS
+// playAgainBtn.addEventListener('click', () => {location.reload()});
+// rematchBtn.addEventListener('click', () => {location.reload()});
+
 playAgainBtn.addEventListener('click', () => {location.reload()});
-rematchBtn.addEventListener('click', () => {location.reload()})
 closeOverlayWin.addEventListener('click', ()=> closeWin());
 closeOverlayLose.addEventListener('click', ()=> closeLose());
 
@@ -54,7 +47,7 @@ function closeWin() {
     overlayWin.classList.toggle('show-win')
 }
 function closeLose() {
-    overlayWin.classList.toggle('show-lose');
+    overlayLose.classList.toggle('show-lose');
 }
 
 alwaysFocusedInput.addEventListener( 'blur',() => { //Fokus på input fält
@@ -65,9 +58,9 @@ alwaysFocusedInput.addEventListener( 'blur',() => { //Fokus på input fält
 
 
 for (i = 0; i < correctAnswer.length; i++) {
-  currentWord.push("_");
+  currentWord.push('_');
 }
-document.getElementById("active-word").innerHTML = currentWord.join(" ");
+document.getElementById('active-word').innerHTML = currentWord.join(" ");
 
 
 function wordLetters(letter) {
@@ -83,70 +76,50 @@ function lettersToGuess() {
   let i;
   let toGuess = 0;
   for (i in currentWord) {
-    if (currentWord[i] === "_")
+    if (currentWord[i] === '_')
       toGuess++;
   }
   return toGuess;
 }
 
 document.querySelector('input').addEventListener('keydown', (event) => {
-  if (event.key >= 'a' && event.key <= 'z') {
-    let letter = event.key.toLowerCase();
-    let lettersGuessed = letter;
-    let i;
-    let letterPosition = wordLetters(lettersGuessed);
+    if (event.key >= 'a' && event.key <= 'z') {
+        let letter = event.key.toLowerCase();
+        let lettersGuessed = letter;
+        let i;
+        let letterPosition = wordLetters(lettersGuessed);
 
     if (letterPosition.length) {
-      for (i = 0; i < letterPosition.length; i++) {
-        currentWord[letterPosition[i]] = lettersGuessed;
-      }
-      document.getElementById("active-word").innerHTML = currentWord.join(" ");
-    } else {
-      if (!pastLetters.includes(letter)) {
-        pastLetters.push(letter);
-        document.getElementById("letters-guessed").innerHTML += lettersGuessed + " ";
-        guessesLeft--;
-        document.getElementById("guesses-remain").innerHTML = guessesLeft;
-      }
-    }
-
-    // YOU WIN OVERLAY
-
-
+        for (i = 0; i < letterPosition.length; i++) {
+            currentWord[letterPosition[i]] = lettersGuessed;
+        }
+        
+        document.getElementById("active-word").innerHTML = currentWord.join(" ");
+    } else if (!pastLetters.includes(letter)) {
+            pastLetters.push(letter);
+            document.getElementById("letters-guessed").innerHTML += lettersGuessed + " ";
+            guessesLeft--;
+            document.getElementById("guesses-remain").innerHTML = guessesLeft;
+            }
+    //YOU WIN
     if (lettersToGuess() == 0) {
         overlayWin.classList.toggle('show-win');
-        
-        guessesLeft = 5;
-        document.getElementById("guesses-remain").innerHTML = guessesLeft;
-
-        document.getElementById("letters-guessed").innerHTML = lettersReset;
-
-        correctAnswer = wordArray[Math.floor(Math.random() * wordArray.length)];
-
-        currentWord = [];
-        pastLetters = [];
-        for (i = 0; i < correctAnswer.length; i++) {
-            currentWord.push("_");
-        }
-        document.getElementById("active-word").innerHTML = currentWord.join(" ");
     }
-
-    //YOU LOSE OVERLAY
+    //YOU LOSE
+    if (guessesLeft == 4) {
+        document.querySelector('figure').classList.add('scaffold');
+    }
+    if (guessesLeft == 3) {
+        document.querySelector('figure').classList.add('head');
+    }
+    if (guessesLeft == 2) {
+        document.querySelector('figure').classList.add('arms');
+    }
+    if (guessesLeft == 1) {
+        document.querySelector('figure').classList.add('legs');
+    }
     if (guessesLeft === 0) {
         overlayLose.classList.toggle('show-lose');
-      guessesLeft = 5;
-      document.getElementById("guesses-remain").innerHTML = guessesLeft;
-
-      document.getElementById("letters-guessed").innerHTML = lettersReset;
-
-      correctAnswer = wordArray[Math.floor(Math.random() * wordArray.length)];
-
-      currentWord = [];
-      pastLetters = [];
-      for (i = 0; i < correctAnswer.length; i++) {
-        currentWord.push("_");
-      }
-      document.getElementById("active-word").innerHTML = currentWord.join(" ");
     }
-  }
+}
 });
